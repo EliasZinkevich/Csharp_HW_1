7a2097a92c7c0567d0b3e5e50ca0621cf33bb0f0
Elias Karjalainen <zinkevich-ilia@yandex.ru>
2023-09-24 21:43:21 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Мил Государь, напиши первшее число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ну, теперича и второе ляпни");
+int B = Convert.ToInt32(Console.ReadLine());
+if(A == B)
+{Console.WriteLine("Чой-то ты мене дуришь, одинаковые они!");}
+else if(A > B)
+{Console.WriteLine("Но так первшее поболе будет");}
+else 
+{Console.WriteLine("Как ни крути, а второе-то бооольше");}
